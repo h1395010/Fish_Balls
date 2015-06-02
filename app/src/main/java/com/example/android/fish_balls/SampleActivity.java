20b15a90df2f6f2c40998924c3042865d506d14f
matthias <h1395010@connect.hku.hk>
2015-06-03 01:00:21 +0800

    first commit

---
+++ app/src/main/java/com/example/android/fish_balls/SampleActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.fish_balls;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+
+public class SampleActivity extends Activity {
+    FlyOutContainer root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        this.root = (FlyOutContainer) this.getLayoutInflater().inflate(R.layout.activity_sample, null);
+
+        this.setContentView(root);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sample, menu);
+        return true;
+    }
+
+    public void toggleMenu(View v){
+        this.root.toggleMenu();
+    }
+
+}
